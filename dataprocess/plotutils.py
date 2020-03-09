c54f7ed387a221761320bbd08a381f9996d62cac
Avisek Naug <aviseknaug1993@gmail.com>
2020-03-09 14:35:41 -0500

    added bar plot

--- dataprocess/plotutils.py
+++ dataprocess/plotutils.py
@@ -1,9 +1,11 @@
 import matplotlib
 from matplotlib import pyplot as plt
+import numpy as np
+from scipy.interpolate import make_interp_spline, BSpline
 
 
 def pred_v_target_plot(timegap, outputdim, output_timesteps, preds, target,
- saveloc, scaling: bool, scaler, lag: int = -1, outputdim_names : list = [], typeofplot: str = 'train'):
+ saveloc, scaling: bool, scaler, lag: int = -1, outputdim_names : list = [], typeofplot: str = 'train', Week: int = 0):
 
 	if not outputdim_names:
 		outputdim_names = ['Output']*outputdim
@@ -18,6 +20,10 @@ def pred_v_target_plot(timegap, outputdim, output_timesteps, preds, target,
 		for j in  range(output_timesteps):
 			preds[:,j,:] = scaler.inverse_transform(preds[:,j,:])
 			target[:,j,:] = scaler.inverse_transform(target[:,j,:])
+
+	# attach forward slash if saveloc does not have one
+	if not saveloc.endswith('/'):
+			saveloc += '/'
 
 
 	# training output
@@ -35,5 +41,45 @@ def pred_v_target_plot(timegap, outputdim, output_timesteps, preds, target,
 			axs[i+j, 0].grid(which='both',alpha=100)
 			axs[i+j, 0].legend()
 			axs[i+j, 0].minorticks_on()
-	fig.savefig(saveloc+str(timegap)+'_LSTM_'+typeofplot+'prediction.pdf', bbox_inches='tight')
+	fig.savefig(saveloc+str(timegap)+'_LSTM_'+typeofplot+'prediction-Week{}.pdf'.format(Week), bbox_inches='tight')
 	plt.close(fig)
+
+def single_bar_plot(bars: list, color, bar_label: str, saveloc: str, barwidth = 0.50, smoothcurve: bool = False,
+ bar_annotate: bool = False, saveplot: bool = False, plot_name: str = 'BarPlot', xlabel: str = 'Xlabel', ylabel: str = 'ylabel',
+ title: str = 'Title', xticktype: str = 'Bar'):
+
+	plt.rcParams["figure.figsize"] = (15,10)
+	font = {'size':16, 'family': "Times New Roman"}
+	plt.rc('font',**font)
+	plt.rc('legend',**{'fontsize':14})
+
+	N = len(bars)
+	ind = np.arange(N)
+
+	plt.bar(ind,
+	 bars, 
+	 barwidth, 
+	 color=color, 
+	 label=bar_label)
+
+	plt.xticks(ind, [xticktype + str(i) for i in range(1, N + 1)], rotation = 45)
+	plt.ylabel(ylabel)
+	plt.xlabel(xlabel)
+	plt.title(title)
+
+	if smoothcurve:
+		T = np.array([i for i in range(len(bars))])
+		xnew = np.linspace(T.min(), T.max(), 300)
+		spl = make_interp_spline(T, bars, k=3)  # type: BSpline
+		power_smooth = spl(xnew)
+		plt.plot(xnew, power_smooth, color='k', alpha=0.8)
+
+	if bar_annotate:
+		for i, v in enumerate(bars):
+			plt.text(i - 0.30, bars[i], '{0:.1f}%'.format(np.abs(v)), color='g', fontweight='bold', fontsize=13)
+
+	if saveplot:
+		# attach forward slash if saveloc does not have one
+		if not saveloc.endswith('/'):
+			saveloc += '/'
+		plt.savefig(saveloc + 'Weekly Energy Savings.png', bbox_inches='tight', dpi=300)
