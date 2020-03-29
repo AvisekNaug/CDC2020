c3b62c9083756ea087dda09405234d7f3d87c6ae
Avisek Naug <aviseknaug1993@gmail.com>
2020-03-29 18:11:44 -0500

    ongoing work for relearning

--- rl_source/relearning.py
+++ rl_source/relearning.py
@@ -11,6 +11,8 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from pandas import DataFrame, concat
 
+# TODO: Set the seeds
+
 with warnings.catch_warnings():
 
 	warnings.filterwarnings("ignore",category=FutureWarning)
@@ -23,11 +25,25 @@ with warnings.catch_warnings():
 
 	from stable_baselines.common.vec_env import SubprocVecEnv, DummyVecEnv
 	from stable_baselines.common import set_global_seeds, make_vec_env
-	
 	from keras.models import load_model
-	from dataprocess import dataprocessor as dp
+
 	from nn_source import models as mp
-	from rl_source import alumnienv
+	from rl_source import alumnienv, controller
+
+# Would want to see the warnings
+from dataprocess import dataprocessor as dp
+from dataprocess import plotutils as pu
+from dataprocess import logutils as lu
+
+# create new directory if it does not exist; eles clear existing files in it
+def make_dir(dir_path):
+	# clear old files
+	try:
+		os.mkdir(dir_path)
+	except FileExistsError:
+		files = os.listdir(dir_path)
+		for f in files:
+			os.remove(dir_path + f)
 
 # return a new column which is the sum of previous window_size values
 def windowsum(df, window_size: int, column_name: str):
@@ -36,9 +52,9 @@ def windowsum(df, window_size: int, column_name: str):
 def quickmerge(listdf):
     return concat(listdf)
 
-def prepare_dflist():
+def prepare_dflist(period):
 
-	period = 6  # the period to sample the data at. 1 period= 5 minutes
+	period = period  # the period to sample the data at. 1 period= 5 minutes
 
 	dfdata = dp.readfile('../data/processed/buildingdata.pkl')  # read the pickled file for ahu data
 	df = dfdata.return_df(processmethods=['file2df'])  # return pickled df
@@ -79,7 +95,7 @@ def dflist2overlap_dflist(dflist, data_weeks):
 
 	for weekdata in dflist[data_weeks:]:
 	
-		datablock = datablock[1:]+[weekdata]  # remove 1st of data from initial_datablock
+		datablock = datablock[1:]+[weekdata]  # remove 1st of data from initial_datablock and add new week of data
 		out_dflist.append(quickmerge(datablock))
 
 	return out_dflist
@@ -106,17 +122,35 @@ def overlap_dflist2array(out_dflist, input_vars, outut_vars,lag,splitvalue, X_sc
 	
 
 
-def main():
+def main(trial: int = 0, adaptive = True):
 
-	"""" Begin : Creating all the data requirements"""
-	totaldf, dflist, dfscaler = prepare_dflist()  # break df to weekly dflist and return scaler for entire data
-	data_weeks=52
-	out_dflist = dflist2overlap_dflist(dflist, data_weeks=data_weeks)  # Create dflist with weekly overlaps
+	period = 6
 
 	cwe_input_vars=['oat', 'orh', 'sat', 'ghi', 'flow'],  # cwe lstm model input variables
 	cwe_outut_vars = ['30min_cwe']  # cwe lstm model input variables
 	hwe_input_vars=['oat','orh', 'sat', 'ghi', 'hw_sf', 'hw_st'],  # hwe lstm model input variables
 	hwe_outut_vars = ['30min_hwe']  # hwe lstm model input variables
+
+	num_rl_steps = 30000  # steps to train the rl agent
+	n_envs = 2  # always make sure that the number of environments is even; can also be os.cpu_count()
+	obs_space_vars=['oat', 'orh', 'ghi', 'sat', 'avg_stpt', 'flow'],  # rl state space
+	action_space_vars=['sat'],  # rl action space
+
+	pathinsert = 'adaptive' if adaptive else 'fixed'  # Decide folder structure based on adaptive vs fixed controller
+
+	model_save_dir='../models/'+pathinsert+'/Trial_{}/'.format(trial)  # save the rl agents here
+	make_dir(model_save_dir)  # create the folder if it does not exist
+
+	log_dir = model_save_dir + '/performance/'  # save the rl performance output here
+	make_dir(log_dir)  # create the folder if it does not exist
+
+	base_log_path = '../log/'+pathinsert+'/'
+
+	
+	#######################         Begin : Creating all the data requirements     ##################################
+	totaldf, dflist, dfscaler = prepare_dflist(period)  # entire df, break df to weekly dflist, scaler for entire data
+	data_weeks=52
+	out_dflist = dflist2overlap_dflist(dflist, data_weeks=data_weeks)  # Create dflist with weekly overlaps
 
 	_, _, _, _, cwe_X_scaler, cwe_y_scaler = dp.df2arrays(totaldf, predictorcols=cwe_input_vars, 
 	outputcols=cwe_outut_vars, feature_range=(0,1), reshaping=False)  # extract cwe scaler for entire data
@@ -130,78 +164,115 @@ def main():
 	hwe_week_list = overlap_dflist2array(out_dflist, hwe_input_vars, hwe_outut_vars, lag=-1, 
 										splitvalue=(data_weeks-1)/data_weeks, X_scale=hwe_X_scaler, y_scale=hwe_y_scaler)
 
-	erromsg = "Unequal lists: len(out_dflist)={0:}, len(out_dflist)={1:}, len(hwe_week_list)={2:}".format(
-		len(out_dflist), len(out_dflist), len(hwe_week_list))
+	erromsg = "Unequal lists: len(out_dflist)={0:}, len(cwe_week_list)={1:}, len(hwe_week_list)={2:}".format(
+		len(out_dflist), len(cwe_week_list), len(hwe_week_list))
 	assert all([len(out_dflist)==len(cwe_week_list), len(out_dflist)==len(hwe_week_list)]), erromsg
-	"""" End : Creating all the data requirements"""
+	#######################         End: Creating all the data requirements      ##################################
 
 
-
-	"""Begin : Prerequisites for the environment"""
-
+	#######################         Begin : Prerequisites for the environment     ##################################
 	# important parameters to scale the reward
 	params = {
-
 		'energy_saved': 1.0, 'energy_savings_thresh': 0.0, 'energy_penalty': -1.0, 'energy_reward_weight': 0.5,
 		'comfort': 1.0, 'comfort_thresh': 0.10, 'uncomfortable': -1.0, 'comfort_reward_weight': 0.5,
 		'action_minmax': [np.array([totaldf.sat.min()]), np.array([totaldf.sat.max()])]  # required for clipping
-
-	}
+		}
 
 	env_id = alumnienv.Env  # the environment ID or the environment class
-	n_envs = 2  # can also be os.cpu_count()
-	"""always make sure that the number of environments is even"""
 	seed = 123  # the initial seed for the random number generator
 	start_index = 0  # start rank index
 	vec_env_cls = SubprocVecEnv  #  A custom `VecEnv` class constructor. Default: DummyVecEnv
-	"""End : Prerequisites for the environment"""
-	
+	#######################         End : Prerequisites for the environment     ##################################
+
+
 	# main iteration loop
-	for out_df, cwe_week, hwe_week in zip(out_dflist, cwe_week_list,hwe_week_list):
+
+	agent_created = False
+	Week = 0
+
+	for out_df, cwe_week, hwe_week in zip(out_dflist, cwe_week_list, hwe_week_list):
 		
 		"""train lstm model on cwe"""
+		# TODO: train the model and return best model so far
+
+		# TODO: load energy model
+		cwe_model = load_model('../results/lstm_cwe_best/LSTM_model_10_0.00')
+
 
 		"""train lstm model on hwe"""
+		# TODO: train the model and return best model so far
+
+
+		# TODO: load energy model
+		hwe_model = load_model('../results/lstm_hwe_best/LSTM_model_42_0.01')
+
+
+		"""create environment with new data"""
+		# Path to a folder where the monitor files will be saved
+		monitor_dir = base_log_path+'Trial_{}/Interval_{}/'.format(trial,Week)
+		
+		# Arguments to be fed to the custom environment inside make_vec_env
+		env_kwargs = dict(  #  Optional keyword argument to pass to the env constructor
+
+			df=dfscaler.transform(out_df),  # the file for iterating; it is scaled before being passed
+			obs_space_vars=obs_space_vars,  # state space variable
+			action_space_vars=action_space_vars,  # action space variable
+			action_space_bounds=[[-2.0], [2.0]],  # bounds for real world action space; is scaled internally using the params
+
+			cwe_energy_model=cwe_model,  # trained lstm model
+			cwe_input_vars=cwe_input_vars,  # lstm model input variables
+			cwe_input_shape=(1, 1, len(cwe_input_vars)),  # lstm model input data shape (no_samples, output_timestep, inputdim)
+
+			hwe_energy_model=hwe_model,  # trained lstm model
+			hwe_input_vars=hwe_input_vars,  # lstm model input variables
+			hwe_input_shape=(1, 1, len(hwe_input_vars)),  # lstm model input data shape (no_samples, output_timestep, inputdim)
+
+			**params  # the reward adjustment parameters
+		)
+
+		# make the environment
+		envmodel = make_vec_env(env_id = env_id, n_envs = n_envs, seed = seed, start_index = start_index,
+						monitor_dir = monitor_dir, vec_env_cls = vec_env_cls, env_kwargs = env_kwargs)
+
+
+		"""provide envrionment to the new or existing rl model"""
+		# create agent with new data or reuse old agent if in the loop for the first time
+		if not agent_created:
+			# create new agent with the environment model
+			agent = controller.get_agent(env=envmodel, model_save_dir=model_save_dir, monitor_log_dir=monitor_dir)
+			
+
 
 		"""train rl model"""
-		# create environment with new data
-		monitor_dir = '../log/Trial_{}/Interval_{}/'.format(0,1)  # Path to a folder where the monitor files will be saved
+		# make sure environment is in train mode
+		print("setting environment to train mode..... \n")
+		envmodel.env_method('trainenv')
 
-		# create agent with new data or reuse old agent if in the loop for the first time
+		if not agent_created | adaptive :
+			# train the agent'+pathinsert+'
+			print("Training Started... \n")
+			agent = controller.train_agent(agent, env = envmodel, steps=num_rl_steps,
+			tb_log_name='../log/ppo2_event_folder{}'.format(trial))
+		else:
+			print('Retraining aborted as fixed controller will be used... \n')
 
-		# train the agent
 
 		"""test rl model"""
-		# load the agent file from previous best
-	
-	# Arguments to be fed to the custom environment inside make_vec_env
-	env_kwargs = dict(  #  Optional keyword argument to pass to the env constructor
+		# make sure environment is in test mode
+		print("setting environment to test mode..... \n")
+		envmodel.env_method('testenv')
 
-		df=dfscaler.transform(out_df),  # the file for iterating; it is scaled before being passed
-		obs_space_vars=['oat', 'orh', 'ghi', 'sat', 'avg_stpt', 'flow'],  # state space variable
-		action_space_vars=['sat'],  # action space variable
-		action_space_bounds=[[-2.0], [2.0]],  # bounds for real world action space; is scaled internally using the params
+		# provide path to the current best rl agent weights and test it
+		best_model_path = model_save_dir + 'best_model.pkl'
+		test_perf_log = controller.test_agent(best_model_path, envmodel, num_episodes=1)
 
-		cwe_energy_model=cwe_model,  # trained lstm model
-		cwe_input_vars=cwe_input_vars,  # lstm model input variables
-		cwe_input_shape=(1, 1, len(cwe_input_vars)),  # lstm model input data shape (no_samples, output_timestep, inputdim)
+		envmodel.close()  # clear the environment and its resources
 
-		hwe_energy_model=hwe_model,  # trained lstm model
-		hwe_input_vars=hwe_input_vars,  # lstm model input variables
-		hwe_input_shape=(1, 1, len(hwe_input_vars)),  # lstm model input data shape (no_samples, output_timestep, inputdim)
+		# save the agent performance on test data
+		lu.rl_perf_save(test_perf_log, log_dir)
 
-		**params  # the reward adjustment parameters
-	)
-	#vec_env_kwargs = dict(start_method="fork")
-
-	envmodel = make_vec_env(env_id = env_id,
-					n_envs = n_envs,
-					seed = seed,
-					start_index = start_index,
-					monitor_dir = monitor_dir,
-					vec_env_cls = vec_env_cls,
-					env_kwargs = env_kwargs)#,
-					#vec_env_kwargs=vec_env_kwargs)
+		Week += 1  # shift to the next week
+		agent_created = True  # flip the agent_created flag
 
 # wrap the environment in the vectorzied wrapper with SubprocVecEnv
 # run the environment inside if __name__ == '__main__':
