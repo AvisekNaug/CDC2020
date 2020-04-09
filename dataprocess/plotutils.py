eec02b9741cc58ab69d05b3ba81826e1ece8d8c3
Avisek Naug <aviseknaug1993@gmail.com>
2020-04-08 19:51:41 -0500

    bug fix: prevent original array overwrite

--- dataprocess/plotutils.py
+++ dataprocess/plotutils.py
@@ -7,47 +7,6 @@ from pandas import read_csv
 
 
 def pred_v_target_plot(timegap, outputdim, output_timesteps, preds, target,
- saveloc, scaling: bool, scaler, lag: int = -1, outputdim_names : list = [], typeofplot: str = 'train', Week: int = 0):
-
-	if not outputdim_names:
-		outputdim_names = ['Output']*outputdim
-
-	plt.rcParams["figure.figsize"] = (15, 5*outputdim*output_timesteps)
-	font = {'size':16}
-	plt.rc('font',**font)
-	plt.rc('legend',**{'fontsize':14})
-
-	# Inerse scaling the data for each time step
-	if scaling:
-		for j in  range(output_timesteps):
-			preds[:,j,:] = scaler.inverse_transform(preds[:,j,:])
-			target[:,j,:] = scaler.inverse_transform(target[:,j,:])
-
-	# attach forward slash if saveloc does not have one
-	if not saveloc.endswith('/'):
-			saveloc += '/'
-
-
-	# training output
-	fig, axs = plt.subplots(nrows = outputdim*output_timesteps, squeeze=False)
-	for i in range(outputdim):
-		for j in range(output_timesteps):
-			# plot predicted
-			axs[i+j, 0].plot(preds[:, j, i], 'r--', label='Predicted'+outputdim_names[i])
-			# plot target
-			axs[i+j, 0].plot(target[:, j, i], 'g--', label='Actual'+outputdim_names[i])
-			# Plot Properties
-			axs[i+j, 0].set_title('Predicted vs Actual at time = t + {} for {}'.format(-1*lag+j, outputdim_names[i]))
-			axs[i+j, 0].set_xlabel('Time points at {} minute(s) intervals'.format(timegap))
-			axs[i+j, 0].set_ylabel('Actual Energy')
-			axs[i+j, 0].grid(which='both',alpha=100)
-			axs[i+j, 0].legend()
-			axs[i+j, 0].minorticks_on()
-	fig.savefig(saveloc+str(timegap)+'_LSTM_'+typeofplot+'prediction-Week{}.pdf'.format(Week), bbox_inches='tight')
-	plt.close(fig)
-
-
-def pred_v_target_plot_transferlearning(timegap, outputdim, output_timesteps, preds, target,
  saveloc, scaling: bool, scaler, lag: int = -1, outputdim_names : list = [], typeofplot: str = 'train', Idx: int = 0):
 
 	if not outputdim_names:
@@ -58,11 +17,14 @@ def pred_v_target_plot_transferlearning(timegap, outputdim, output_timesteps, pr
 	plt.rc('font',**font)
 	plt.rc('legend',**{'fontsize':14})
 
+	_preds = np.empty_like(preds)
+	_target = np.empty_like(target)
+
 	# Inerse scaling the data for each time step
 	if scaling:
 		for j in  range(output_timesteps):
-			preds[:,j,:] = scaler.inverse_transform(preds[:,j,:])
-			target[:,j,:] = scaler.inverse_transform(target[:,j,:])
+			_preds[:,j,:] = scaler.inverse_transform(preds[:,j,:])
+			_target[:,j,:] = scaler.inverse_transform(target[:,j,:])
 
 	# attach forward slash if saveloc does not have one
 	if not saveloc.endswith('/'):
@@ -74,9 +36,9 @@ def pred_v_target_plot_transferlearning(timegap, outputdim, output_timesteps, pr
 	for i in range(outputdim):
 		for j in range(output_timesteps):
 			# plot predicted
-			axs[i+j, 0].plot(preds[:, j, i], 'r--', label='Predicted'+outputdim_names[i])
+			axs[i+j, 0].plot(_preds[:, j, i], 'r--', label='Predicted'+outputdim_names[i])
 			# plot target
-			axs[i+j, 0].plot(target[:, j, i], 'g--', label='Actual'+outputdim_names[i])
+			axs[i+j, 0].plot(_target[:, j, i], 'g--', label='Actual'+outputdim_names[i])
 			# Plot Properties
 			axs[i+j, 0].set_title('Predicted vs Actual at time = t + {} for {}'.format(-1*lag+j, outputdim_names[i]))
 			axs[i+j, 0].set_xlabel('Time points at {} minute(s) intervals'.format(timegap))
@@ -84,7 +46,63 @@ def pred_v_target_plot_transferlearning(timegap, outputdim, output_timesteps, pr
 			axs[i+j, 0].grid(which='both',alpha=100)
 			axs[i+j, 0].legend()
 			axs[i+j, 0].minorticks_on()
-	fig.savefig(saveloc+str(timegap)+'_LSTM_'+typeofplot+'prediction-{}.pdf'.format(Idx), bbox_inches='tight')
+	fig.savefig(saveloc+str(timegap)+'min_LSTM_'+typeofplot+'_prediction-{}.pdf'.format(Idx), bbox_inches='tight')
+	plt.close(fig)
+    
+def detailedplot(timegap, xs, outputdim, output_timesteps, input_timesteps, pred, target, X_var,
+saveloc, scaling: bool, Xscaler, yscaler, lag: int = -1, outputdim_names : list = [], typeofplot: str = 'train', 
+				Idx: str = ''):
+	
+	if not outputdim_names:
+		outputdim_names = ['Output']*outputdim
+		
+	_pred = np.empty_like(pred)
+	_target = np.empty_like(target)
+	_X_var = np.empty_like(X_var)
+
+	plt.rcParams["figure.figsize"] = (15, 5*outputdim*output_timesteps)
+	font = {'size':16}
+	plt.rc('font',**font)
+	plt.rc('legend',**{'fontsize':14})
+
+	# Inerse scaling the data for each time step
+	if scaling:
+		for j in  range(output_timesteps):
+			_pred[:,j,:] = yscaler.inverse_transform(pred[:,j,:])
+			_target[:,j,:] = yscaler.inverse_transform(target[:,j,:])
+		for j in  range(input_timesteps):
+			_X_var[:,j,:] = Xscaler.inverse_transform(X_var[:,j,:])     
+
+	# attach forward slash if saveloc does not have one
+	if not saveloc.endswith('/'):
+			saveloc += '/'
+
+
+	# training output
+	fig, axs = plt.subplots(nrows = outputdim*output_timesteps, squeeze=False)
+	for i in range(outputdim):
+		for j in range(output_timesteps):
+			# plot predicted
+			axs[i+j, 0].plot_date(xs, _pred[:, j, i], 'ro-', label='Predicted'+outputdim_names[i])
+			# plot target
+			axs[i+j, 0].plot_date(xs, _target[:, j, i], 'g*-', label='Actual'+outputdim_names[i])
+			# plot other variables: temperature
+			axs[i+j, 0].plot_date(xs, _X_var[:, 0, 0], 'b3-', label='Outside Air Temperature')
+			# plot other variables: relative humidity
+			axs[i+j, 0].plot_date(xs, _X_var[:, 0, 1], 'm4-', label='Outside Air Rel Humidity')
+			# plot other variables: Supply Air Temperature
+			axs[i+j, 0].plot_date(xs, _X_var[:, 0, 2], 'k^-', label='Supply Air Temperature')
+			# plot other variables: flow value
+			axs[i+j, 0].plot_date(xs, _X_var[:, 0, 4], 'c>-', label='Hot Water Flow Rate')
+			# Plot Properties
+			axs[i+j, 0].set_title('Predicted vs Actual at time = t + {} for {}'.format(-1*lag+j, outputdim_names[i]))
+			axs[i+j, 0].set_xlabel('Time points at {} minute(s) intervals'.format(timegap))
+			axs[i+j, 0].set_ylabel('Different Variables')
+			axs[i+j, 0].grid(which='both',alpha=100)
+			axs[i+j, 0].minorticks_on()
+			axs[i+j, 0].legend(loc='upper left', bbox_to_anchor=(1.0, 1.0))
+	fig.savefig(saveloc+str(timegap)+'min_LSTM_detailed_'+typeofplot+'_plot-{}.pdf'.format(Idx), bbox_inches='tight')
+	plt.gcf().autofmt_xdate()
 	plt.close(fig)
 
 
@@ -132,7 +150,7 @@ def single_bar_plot(bars: list, color, bar_label: str, saveloc: str, barwidth = 
 		# attach forward slash if saveloc does not have one
 		if not saveloc.endswith('/'):
 			saveloc += '/'
-		plt.savefig(saveloc + 'ErrorPlot.png', bbox_inches='tight', dpi=300)
+		plt.savefig(saveloc + 'Error Bar Plot.png', bbox_inches='tight', dpi=300)
 
 
 def reward_agg_plot(trial_list: list, 
